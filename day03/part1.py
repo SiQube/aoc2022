845a41bd9017fa9ae024d217cabaf4607347f8b8
SiQube <reich.davidr@gmail.com>
2022-12-03 13:44:17 +0100

    solution day3 part1

---
+++ day03/part1.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import os.path
+
+import pytest
+
+
+_INPUT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def _solve(inp: str) -> int:
+    score = 0
+    for line in inp.splitlines():
+        first_half, second_half = line[:len(line)//2], line[len(line)//2:]
+        letter = list(set(first_half).intersection(set(second_half)))[0]
+        if letter == letter.capitalize():
+            letter_score = ord(letter) - 38
+        else:
+            letter_score = ord(letter) - 96
+        score += letter_score
+
+    return score
+
+
+_TESTS = """\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"""
+_EXPECTED = 157
+
+
+@pytest.mark.parametrize(
+    ('input_str', 'expected'),
+    (
+        (_TESTS, _EXPECTED),
+    ),
+)
+def test(input_str: str, expected: int) -> None:
+    assert _solve(input_str) == expected
+
+
+def main() -> int:
+    with open(_INPUT) as fp:
+        print(_solve(fp.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
