5b153754202c6a5d6522b0b591e3c2f722be1885
SiQube <reich.davidr@gmail.com>
2022-12-13 19:34:08 +0100

    solution day10 part1

---
+++ day10/part1.py
@@ -0,0 +1,208 @@
+from __future__ import annotations
+
+import os.path
+
+import pytest
+
+
+_INPUT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def _solve(inp: str) -> int:
+    X = [1]
+    cycle_counter = 0
+    crn_sgn_str_list = []
+    for line in inp.splitlines():
+        try:
+            addx, value = line.split()
+        except ValueError:
+            cycle_counter += 1
+            if ((cycle_counter == 20) or ((cycle_counter - 20) % 40 == 0)):
+                crrnt_sgn_strngth = sum(X) * cycle_counter
+                crn_sgn_str_list.append(crrnt_sgn_strngth)
+            if cycle_counter >= 220:
+                break
+            continue
+        int_val = int(value)
+        for _ in range(2):
+            cycle_counter += 1
+            if ((cycle_counter == 20) or ((cycle_counter - 20) % 40 == 0)):
+                crrnt_sgn_strngth = sum(X) * cycle_counter
+                crn_sgn_str_list.append(crrnt_sgn_strngth)
+        if cycle_counter >= 220:
+            break
+        X.append(int_val)
+
+    return sum(crn_sgn_str_list)
+
+
+_TESTS = """\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
+_EXPECTED = 13140
+
+
+@pytest.mark.parametrize(
+    ('input_str', 'expected'),
+    (
+        (_TESTS, _EXPECTED),
+    ),
+)
+def test(input_str: str, expected: int) -> None:
+    assert _solve(input_str) == expected
+
+
+def main() -> int:
+    with open(_INPUT) as fp:
+        print(_solve(fp.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
